68dd99978cb32151be5cf63365951f3ff5235c82
Meirzv <mz2530@nyu.edu>
2019-11-02 16:01:30 -0400

    add history page

--- app/models.py
+++ app/models.py
@@ -13,6 +13,7 @@ class LoginUser(db.Model, UserMixin):
     authenticated = db.Column(db.Boolean, default=False)
     admin = db.Column(db.Boolean, default=False)
     pw_hash = db.Column(db.String)
+    spell_query = db.Column(db.String, default=None)
 
     def is_active(self):
         return True
@@ -35,3 +36,16 @@ class LoginUser(db.Model, UserMixin):
     def check_password(self, password):
         return check_password_hash(self.pw_hash, password)
 
+    def set_spell_query(self, query):
+        old_query = self.get_spell_query()
+        if old_query is None:
+            # first query for this user
+            print("test first")
+            self.spell_query = query
+        else: #not the first query
+            print("test second")
+            new_query = str(old_query) + "," + query
+            self.spell_query = new_query
+
+    def get_spell_query(self):
+        return self.spell_query
