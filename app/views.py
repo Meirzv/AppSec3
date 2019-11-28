24c294193ee60798c9890a088257899d2920b308
Meirzv <mz2530@nyu.edu>
2019-11-28 15:39:14 -0500

    Delete DB

--- app/views.py
+++ app/views.py
@@ -127,7 +127,6 @@ def index():
 
 @app.route('/history', methods=['GET', 'POST'])
 def history():
-    global name
     if not current_user.is_authenticated:
         return redirect(url_for('login'))
     elif current_user.is_admin():
